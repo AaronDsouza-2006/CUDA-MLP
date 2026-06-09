38bb5b2c408b6e4676c83391bef0955a70af246f
Aaron Dsouza <aaron.c.dsouza2006@gmail.com>
2026-06-09 08:09:15 +0000

    CPU backward prop complete

--- MLP_no_cuda.cpp
+++ MLP_no_cuda.cpp
@@ -41,16 +41,16 @@ void MLP_no_cuda::forward(float *x){
 }
 
 float* MLP_no_cuda::softmax(float* logits){
-    int max_val_idx = argmax(logits, output_size);
+    int max_val_idx = argmax(logits, output);
     float sum = 0, *probs;
-    probs = (float*) malloc(output_size*sizeof(float));
+    probs = (float*) malloc(output*sizeof(float));
 
-    for(int i=0; i<output_size; i++){
+    for(int i=0; i<output; i++){
         probs[i] = std::exp(logits[i]-logits[max_val_idx]);
         sum += probs[i];
     }
 
-    for(int i=0; i<output_size; i++) probs[i]/=sum;
+    for(int i=0; i<output; i++) probs[i]/=sum;
 
     return probs;
 }
@@ -107,6 +107,60 @@ float MLP_no_cuda::cross_entropy_batch(float* probs, int* labels){
     return loss / batch_size;
 }
 
+void MLP_no_cuda::backward(float* x, float* probs, int* labels){
+    float *dlogits = (float*) malloc(output * batch_size * sizeof(float));
+    float *dW2 = (float*) malloc(output * hidden * sizeof(float));
+    float *db2 = (float*) malloc(output * sizeof(float));
+    float *dh = (float*) malloc(hidden * batch_size * sizeof(float));
+    float *dh_pre = (float*) malloc(hidden * batch_size * sizeof(float));
+    float *dW1 = (float*) malloc(hidden * input * sizeof(float));
+    float *db1 = (float*) malloc(hidden * sizeof(float));
+    
+    //compute dlogits
+    for(int i=0; i< output*batch_size; i++)
+        dlogits[i] = probs[i];
+    for(int b=0; b<batch_size; b++)
+        dlogits[labels[b]*batch_size + b] -= 1.0f;
+
+    //compute dW2
+    matmul_transpose_right(output, batch_size, hidden, batch_size, dlogits, h, dW2);
+    //compute db2
+    sum_batch(dlogits, output, db2);
+    //compute dh
+    matmul_transpose_left(output, hidden, output, batch_size, W2, dlogits, dh);
+    //compute dh_pre
+    relu_deriv(h_pre, dh_pre, hidden*batch_size, dh);
+    //compute dW1
+    matmul_transpose_right(hidden, batch_size, input, batch_size, dh_pre, x, dW1);
+    //compute db1
+    sum_batch(dh_pre, hidden, db1);
+
+    //batch averaging and multiply with learning rate
+    scale(dW1, hidden*input, -1.0f/batch_size*lr);
+    scale(db1, hidden, -1.0f/batch_size*lr);
+    scale(dW2, output*hidden, -1.0f/batch_size*lr);
+    scale(db2, output, -1.0f/batch_size*lr);
+
+    //Mini-Batch GD
+    // W1[i] -= lr*dW1[i];
+    // W2[i] -= lr*dW2[i];
+    // b1[i] -= lr*db1[i];
+    // b2[i] -= lr*db2[i];
+    add(hidden*input, W1, dW1);
+    add(hidden, b1, db1);
+    add(output*hidden, W2, dW2);
+    add(output, b2, db2);
+
+    free(dlogits);
+    free(dW2);
+    free(db2);
+    free(dh);
+    free(dh_pre);
+    free(dW1);
+    free(db1);
+}
+
+
 float* MLP_no_cuda::get_logits(){
     return logits;
 }
@@ -136,9 +190,14 @@ void MLP_no_cuda::relu(float* x, int n, float *y){
         y[i] = (x[i] < 0.0f) ? 0.0f : x[i];
 }
 
+
+void MLP_no_cuda::relu_deriv(float* x, float* dx, int n, float *y){
+    for(int i=0; i<n; i++)
+        dx[i] = (x[i] <= 0.0f) ? 0.0f : y[i];
+}
+
 void MLP_no_cuda::matmul(int A, int B, int M, int N, float *x, float *y, float *z){
-    if(B != M)
-        return;
+    if(B != M) return;
 
     for(int row = 0; row < A; row++)
     {
@@ -152,4 +211,65 @@ void MLP_no_cuda::matmul(int A, int B, int M, int N, float *x, float *y, float *
             z[row * N + col] = sum;
         }
     }
+}
+
+void MLP_no_cuda::matmul_transpose_right(int A, int B, int M, int N, float *x, float *y, float *z){
+    // X : A × B
+    // Y : M × N
+    // Y^T : N x M
+    // Z = X @ Y^T
+    // Z : A × M
+    if(B!=N) return;
+    for(int row = 0; row < A; row++)
+    {
+        for(int col = 0; col < M; col++)
+        {
+            float sum = 0.0f;
+
+            for(int k = 0; k < B; k++)
+                sum +=x[row * B + k] * y[col * B + k];
+
+            z[row * M + col] = sum;
+        }
+    }
+}
+
+void MLP_no_cuda::matmul_transpose_left(int A, int B, int M, int N, float *x, float *y, float *z){
+    // X : A × B
+    // X^T : B x A
+    // Y : M × N
+    // Z = X^T @ Y
+    // Z : A × M
+    if(A!=M) return;
+
+    for(int row = 0; row < B; row++)
+    {
+        for(int col = 0; col < N; col++)
+        {
+            float sum = 0.0f;
+            
+            for(int k = 0; k < A; k++)
+                sum += x[k * B + row] * y[k * N + col];
+
+            z[row * N + col] = sum;
+        }
+    }
+}
+
+void MLP_no_cuda::sum_batch(float* x, int n, float *y){
+    for(int i=0; i<n; i++){
+        y[i] = 0;
+        for(int b=0; b<batch_size; b++)
+            y[i] += x[i*batch_size + b];
+    }
+}
+
+void MLP_no_cuda::scale(float* x, int n, float value){
+    for(int i=0; i<n; i++)
+        x[i] *=value;
+}
+
+void MLP_no_cuda::add(int n, float *x, float *y){
+    for(int i=0; i<n; i++)
+        x[i] = x[i] + y[i];
 }
--- MLP_no_cuda.hpp
+++ MLP_no_cuda.hpp
@@ -1,8 +1,6 @@
 #ifndef MLP_no_cuda_CUH
 #define MLP_no_cuda_CUH
 
-#include <cuda_runtime.h>
-
 class MLP_no_cuda{
 public:
     MLP_no_cuda(int input_size, int hidden_size, int output_size, int input_batch_size);
@@ -20,17 +18,26 @@ public:
     float cross_entropy(float* probs, int label);
     float cross_entropy_batch(float* probs, int* labels);
 
+    void backward(float *x, float *probs, int * labels);
+
 private:
         int input, hidden, output; //sizes
         int batch_size;
         float *W1, *W2; //weights
         float *b1, *b2; //biases
         float *h_pre, *h, *logits;
+        float lr = 0.03f;
 
         void initializeWeights();
         void bc_add(float* mat, float* vec, int rows, int cols);
         void relu(float* x, int n, float *y);
         void matmul(int A, int B, int M, int N, float *x, float *y, float *z);
+        void matmul_transpose_right(int A, int B, int M, int N, float *x, float *y, float *z);
+        void matmul_transpose_left(int A, int B, int M, int N, float *x, float *y, float *z);
+        void sum_batch(float* x, int n, float *y);
+        void scale(float* x, int n, float value);
+        void relu_deriv(float* x, float* dx, int n, float *y);
+        void add(int n, float *x, float *y);
 };
 
 #endif
